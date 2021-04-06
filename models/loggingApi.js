0f83337ca4b886794f8ee5d2412393b3f1ac1e82
LSanchez17 <galaxyplayeremail@gmail.com>
2021-04-06 10:41:30 -0400

    data upload route setup

---
+++ models/loggingApi.js
@@ -0,0 +1,7 @@
+const db = require('../db');
+
+class loggingApi {
+
+};
+
+module.exports = loggingApi;
---
+++ routes/logging.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const loggingApi = require('../models/loggingApi');
+
+const router = new express.Router();
+
+/*
+    Logs user visits, voices log transcripts etc
+    data deleted every sunday!
+    Used for showing most common searches, etc.
+*/
+
+router.get('/', async (req, res, next) => {
+    try{
+        //get a list of visitors, voice logs, etc for front end manipulation
+        let loggedData = await loggingApi.getAll();
+
+        return res.json({loggedData});
+    }
+    catch(e){
+        return next(e);
+    }
+});
+
+router.post('/', async (req, res, next) => {
+    try{
+        //get data, and post it user visit, etc
+        let userVisit = await loggingApi.postVisit();
+        
+        return;
+    }
+    catch(e){
+        return next(e);
+    }
+})
+
+
+
+module.exports = router;
